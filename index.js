6c8b481c8e685419addb75e0053795cd55567df3
Victor SV <Victorsegurvieira@gmail.com>
2022-07-21 16:46:40 -0300

    Atividade-JS

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-let lp = ["Peça 1", "Peça 2", "Peça 3", "Peça 4", "Peça 5", "Peça 6", "Peça 7", "Peça 8", "Peça 9"];
+let lp = [ "Peça 1", "Peça 2", "Peça 3", "Peça 4", "Peça 5", "Peça 6", "Peça 7", "Peça 8", "Peça 9" ];
 let qp = lp.length;
-if (qp < 10) {
-	lp.push("Peça 10");
-	console.log(lp);
-} else {
-	console.log("Quantidade máxima de peças atingida!");
+if ( qp < 10 ) {
+	console.log( "Erro na contagem de peças!" );
+}
+for( let i = 0; i < qp; i++ ){
+	let peca = lp[i];
+	if( peca.length < 3 ){
+		console.log( "Erro de caractéres encontrado!" );
+	}
 }
